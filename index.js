fda0833ced11a8bb1928b9eb7472eca620c44d92
jgreen <jgreen@users.noreply.github.com>
2022-05-07 19:47:14 -0700

    CORS policy

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 
 let list = [];
